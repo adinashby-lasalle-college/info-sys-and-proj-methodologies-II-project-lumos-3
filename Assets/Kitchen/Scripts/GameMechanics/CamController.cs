27b60bb048df60514c1220039f50d4c3f8f46b3b
dgdoyh <dgdoyh@gmail.com>
2025-04-21 17:42:16 -0400

    Limited Camera Movement in Kitchen

--- Assets/Kitchen/Scripts/GameMechanics/CamController.cs
+++ Assets/Kitchen/Scripts/GameMechanics/CamController.cs
@@ -5,9 +5,26 @@ public class CamController : MonoBehaviour
     [SerializeField] private InputReader inputReader;
     [SerializeField] private float speed = 3f;
 
+    private float minZ = -1.3f;
+    private float maxZ = 0.6f;
+
     void Update()
     {
+        float currX = transform.position.x;
+        float currY = transform.position.y;
+
         Vector3 moveDir = new Vector3(0f, 0f, inputReader.GetZInput());
         transform.position += moveDir * speed * Time.deltaTime;
+
+        // Block camera to move further than the limited Z
+        if (transform.position.z < minZ)
+        {
+            transform.position = new Vector3(currX, currY, minZ);
+        }
+
+        if (transform.position.z > maxZ)
+        {
+            transform.position = new Vector3(currX, currY, maxZ);
+        }
     }
 }
