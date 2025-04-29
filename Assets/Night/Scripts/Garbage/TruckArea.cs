55a2efa16e782254536799e49177a9c6c6ab615a
Hariiiy <102526866+Hariiiy@users.noreply.github.com>
2025-04-29 17:50:53 -0400

    Add Menu and coin ui for street

--- Assets/Night/Scripts/Garbage/TruckArea.cs
+++ Assets/Night/Scripts/Garbage/TruckArea.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class TruckArea : MonoBehaviour
 {
     [SerializeField] Animator Animator;
+    [SerializeField] GameObject Partcle;
 
     public void DeliverNpc()
     {
         Debug.Log("Deliver NPC");
-        //MoneyManager.Instance.AddMoney(5);
+        MoneyManager.Instance.AddMoney(5);
         AudioPlayer player = gameObject.GetComponent<AudioPlayer>();
         player.PlaySound(0);
         Animator.Play("Deliver");
+        StartCoroutine(DisplayParticle());
 
         GameObject Manager = GameObject.FindGameObjectWithTag("GameManager");
         if (Manager != null)
@@ -20,5 +23,12 @@ public class TruckArea : MonoBehaviour
             //Game Over
             Manager.GetComponent<NewsPaperGenerator>().SwitchNews(3);
         }
+    }
+
+    IEnumerator DisplayParticle()
+    {
+        Partcle.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        Partcle.SetActive(false);
     }
 }
